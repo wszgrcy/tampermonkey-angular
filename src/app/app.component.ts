a22297c0ab1db9195f28f0cb7ce651a2261748f0
chen <wszgrcy@gmail.com>
2018-12-07 20:21:57 +0800

    变更检测

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewEncapsulation, Input } from '@angular/core';
+import { Component, ViewEncapsulation, Input, ChangeDetectorRef } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +11,14 @@ export class AppComponent {
   /**用于传入油猴的跨域请求方法使用 */
   @Input() gmHttp
   title = '自定义元素初始化';
-  constructor() {
-
+  constructor(private http: HttpClient, private cd: ChangeDetectorRef) {
+    //doc 当改变的值需要显示时,请启用强制变更检测
+    cd.detectChanges()
   }
   ngOnInit(): void {
     console.log(this.gmHttp)
     console.log(unsafeWindow)
     console.log(window)
   }
+
 }
